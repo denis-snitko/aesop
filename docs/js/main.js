dc6a6d368b7529f675651a6ab095616f4aa11897
Denis Snitko <denissnitko@gmail.com>
2020-07-24 15:21:35 +0300

    Add swiper section

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,1 +1,29 @@
+let swiper = new Swiper('.swiper-container', {
+    spaceBetween: 20,
+    slidesPerView: 1,
+    loop: true,
+    
+    scrollbar: {
+        el: '.swiper-scrollbar',
+        hide: false,
+    },
+    breakpoints: {
+        640: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 20,
+        },
+        1024: {
+            slidesPerView: 4,
+            spaceBetween: 20,
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
+        },
+    }
 
+});
