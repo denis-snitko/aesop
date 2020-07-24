cb63b19fe1cf2c45dda176e7445677d034799622
Denis Snitko <denissnitko@gmail.com>
2020-07-24 16:20:30 +0300

    Update

--- #src/js/main.js
+++ #src/js/main.js
@@ -1,29 +1,31 @@
-let swiper = new Swiper('.swiper-container', {
-    spaceBetween: 20,
-    slidesPerView: 1,
-    loop: true,
-    
-    scrollbar: {
-        el: '.swiper-scrollbar',
-        hide: false,
-    },
-    breakpoints: {
-        640: {
-            slidesPerView: 2,
-            spaceBetween: 20,
-        },
-        768: {
-            slidesPerView: 3,
-            spaceBetween: 20,
-        },
-        1024: {
-            slidesPerView: 4,
-            spaceBetween: 20,
-            navigation: {
-                nextEl: '.swiper-button-next',
-                prevEl: '.swiper-button-prev',
-            },
-        },
-    }
+let baseSlider = document.querySelectorAll('.swiper-container');
+baseSlider.forEach((el) => {
+    let swiperProduction = new Swiper(el, {
+        spaceBetween: 20,
+        slidesPerView: 1,
+        loop: true,
 
-});
+        scrollbar: {
+            el: '.swiper-scrollbar',
+            hide: false,
+        },
+        breakpoints: {
+            640: {
+                slidesPerView: 2,
+                spaceBetween: 20,
+            },
+            768: {
+                slidesPerView: 3,
+                spaceBetween: 20,
+            },
+            1024: {
+                slidesPerView: 4,
+                spaceBetween: 20,
+                navigation: {
+                    prevEl: '.swiper-button-prev',
+                    nextEl: '.swiper-button-next'
+                },
+            },
+        }
+    });
+})
