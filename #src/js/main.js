b3c7bc3456f88aa4454d6d10bdfe7da2b22e4975
Denis Snitko <denissnitko@gmail.com>
2020-09-01 16:14:42 +0300

    Checkout

--- #src/js/main.js
+++ #src/js/main.js
@@ -1,152 +1,170 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-    let baseSlider = document.querySelectorAll('.swiper-container');
+  let baseSlider = document.querySelectorAll('.swiper-container');
 
-    baseSlider.forEach((el) => {
-        let swiperProduction = new Swiper(el, {
-            spaceBetween: 20,
-            slidesPerView: 1,
-            loop: true,
+  baseSlider.forEach((el) => {
+    let swiperProduction = new Swiper(el, {
+      spaceBetween: 20,
+      slidesPerView: 1,
+      loop: true,
 
-            scrollbar: {
-                el: '.swiper-scrollbar',
-                hide: false,
-            },
-            breakpoints: {
-                640: {
-                    slidesPerView: 2,
-                    spaceBetween: 20,
-                },
-                768: {
-                    slidesPerView: 3,
-                    spaceBetween: 20,
-                },
-                1024: {
-                    slidesPerView: 4,
-                    spaceBetween: 20,
-                    navigation: {
-                        prevEl: '.swiper-button-prev',
-                        nextEl: '.swiper-button-next'
-                    },
-                },
-            }
-        });
-    })
-
-    let ourstorySwiper = new Swiper('.p-ourstory__swiper-container', {
-        slidesPerView: 1,
-
-        navigation: {
+      scrollbar: {
+        el: '.swiper-scrollbar',
+        hide: false,
+      },
+      breakpoints: {
+        640: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+        },
+        768: {
+          slidesPerView: 3,
+          spaceBetween: 20,
+        },
+        1024: {
+          slidesPerView: 4,
+          spaceBetween: 20,
+          navigation: {
             prevEl: '.swiper-button-prev',
             nextEl: '.swiper-button-next'
+          },
         },
-
-        scrollbar: {
-            el: '.swiper-scrollbar',
-            hide: false,
-        }
+      }
     });
+  })
 
-    // Animation Label
-    const inputMail = document.querySelectorAll('.login-modal__email');
+  let ourstorySwiper = new Swiper('.p-ourstory__swiper-container', {
+    slidesPerView: 1,
 
-    inputMail.forEach(element => {
-        element.addEventListener('focus', (event) => {
-            event.target.previousElementSibling.classList.add('login-modal__label--top');
-        })
+    navigation: {
+      prevEl: '.swiper-button-prev',
+      nextEl: '.swiper-button-next'
+    },
+
+    scrollbar: {
+      el: '.swiper-scrollbar',
+      hide: false,
+    }
+  });
+
+  // Animation Label
+  const inputMail = document.querySelectorAll('.login-modal__email');
+
+  inputMail.forEach(element => {
+    element.addEventListener('focus', (event) => {
+      event.target.previousElementSibling.classList.add('login-modal__label--top');
     })
-   
-    // Animation Label (checkout)
-    const checkoutInput = document.querySelectorAll('.s-checkout-form__input');
+  })
 
-    checkoutInput.forEach(element => {
-        element.addEventListener('focus', (event) => {
-            event.target.previousElementSibling.classList.add('s-checkout-form__label--top');
-        })
-    })
-    
-    const checkoutSelect = document.querySelectorAll('.s-checkout-form__select');
+  // Animation Label (checkout)
+  const checkoutInput = document.querySelectorAll('.s-checkout-form__input');
 
-    checkoutSelect.forEach(element => {
-        element.addEventListener('focus', (event) => {
-            event.target.previousElementSibling.classList.add('s-checkout-form__label--top');
-        })
+  checkoutInput.forEach(element => {
+    element.addEventListener('focus', (event) => {
+      event.target.previousElementSibling.classList.add('s-checkout-form__label--top');
     })
+  })
+
+  const checkoutSelect = document.querySelectorAll('.s-checkout-form__select');
+
+  checkoutSelect.forEach(element => {
+    element.addEventListener('focus', (event) => {
+      event.target.previousElementSibling.classList.add('s-checkout-form__label--top');
+    })
+  })
 });
 
 jQuery(function ($) {
-    const
-        $cartBtn = $('.cart-btn'),
-        $cart = $('.s-cart'),
-        $sCartClose = $('.s-cart__close'),
-        $sTopHeader = $('.s-top-header'),
-        $sDelivery = $('.s-delivery'),
-        $sDeliveryClose = $('.s-delivery__close'),
-        $mainMenuLink = $('.main-menu__item'),
-        $burgerMenu = $('.burger-menu'),
-        $burgerMenuMainItem_1 = $('.burger-menu__main-item--1'),
-        $burgerMenuMainItem_2 = $('.burger-menu__main-item--2'),
-        $burgerMenuItem = $('.burger-menu__item'),
-        $burgerMenuClose = $('.burger-menu__close');
+  const
+    $cartBtn = $('.cart-btn'),
+    $cart = $('.s-cart'),
+    $sCartClose = $('.s-cart__close'),
+    $sTopHeader = $('.s-top-header'),
+    $sDelivery = $('.s-delivery'),
+    $sDeliveryClose = $('.s-delivery__close'),
+    $mainMenuLink = $('.main-menu__item'),
+    $burgerMenu = $('.burger-menu'),
+    $burgerMenuMainItem_1 = $('.burger-menu__main-item--1'),
+    $burgerMenuMainItem_2 = $('.burger-menu__main-item--2'),
+    $burgerMenuItem = $('.burger-menu__item'),
+    $burgerMenuClose = $('.burger-menu__close');
 
-    $cartBtn.on('click', () => {
-        $cart.show(270);
-        $sTopHeader.hide();
-    });
+  $cartBtn.on('click', () => {
+    $cart.show(270);
+    $sTopHeader.hide();
+  });
 
-    $sCartClose.on('click', () => {
-        $cart.hide(270);
-        $sTopHeader.show();
-    })
+  $sCartClose.on('click', () => {
+    $cart.hide(270);
+    $sTopHeader.show();
+  })
 
-    $sTopHeader.on('click', () => {
-        $sDelivery.show(270);
-        $('body').attr('style', 'overflow: hidden');
-    })
+  $sTopHeader.on('click', () => {
+    $sDelivery.show(270);
+    $('body').attr('style', 'overflow: hidden');
+  })
 
-    $sDeliveryClose.on('click', () => {
-        $sDelivery.hide(270);
-        $('body').attr('style', 'overflow: auto');
-    })
+  $sDeliveryClose.on('click', () => {
+    $sDelivery.hide(270);
+    $('body').attr('style', 'overflow: auto');
+  })
 
-    $mainMenuLink.on('click', () => {
-        $burgerMenuMainItem_1.attr('style', 'display: flex');
-        $burgerMenu.show(320);
-        $burgerMenuMainItem_1.show(320);
-        $('body').attr('style', 'overflow: hidden');
-    })
+  $mainMenuLink.on('click', () => {
+    $burgerMenuMainItem_1.attr('style', 'display: flex');
+    $burgerMenu.show(320);
+    $burgerMenuMainItem_1.show(320);
+    $('body').attr('style', 'overflow: hidden');
+  })
 
-    $burgerMenuItem.on('click', () => {
-        $burgerMenuMainItem_2.show(320);
-        $burgerMenuMainItem_2.attr('style', 'display: flex');
-        // $('body').attr('style', 'overflow: hidden');
-    })
+  $burgerMenuItem.on('click', () => {
+    $burgerMenuMainItem_2.show(320);
+    $burgerMenuMainItem_2.attr('style', 'display: flex');
+    // $('body').attr('style', 'overflow: hidden');
+  })
 
-    $burgerMenuClose.on('click', () => {
-        $burgerMenu.hide(320);
-        $burgerMenuMainItem_1.hide();
-        $burgerMenuMainItem_2.hide();
-        $('body').attr('style', 'overflow: auto');
-    })
+  $burgerMenuClose.on('click', () => {
+    $burgerMenu.hide(320);
+    $burgerMenuMainItem_1.hide();
+    $burgerMenuMainItem_2.hide();
+    $('body').attr('style', 'overflow: auto');
+  })
 
-    //** Scroll */
-    $('.p-ourstory__btn').on('click', function () {
-        let $dest = $('.p-ourstory-history'); // куда скролим
-        if ($dest !== undefined && $dest !== '') { // проверяем существование
-            $('html').animate({
-                scrollTop: $($dest).offset().top // прокручиваем страницу к требуемому элементу
-            }, 500 // скорость прокрутки
-            );
-        }
-        return false;
-    });
+  //** Scroll */
+  $('.p-ourstory__btn').on('click', function () {
+    let $dest = $('.p-ourstory-history'); // куда скролим
+    if ($dest !== undefined && $dest !== '') { // проверяем существование
+      $('html').animate({
+          scrollTop: $($dest).offset().top // прокручиваем страницу к требуемому элементу
+        }, 500 // скорость прокрутки
+      );
+    }
+    return false;
+  });
 
 
-    //** Accordeon  */
-    const $accordeonContTitle = $('.accordeon__content-title');
+  //** Accordeon  */
+  const $accordeonContTitle = $('.accordeon__content-title');
 
-    $accordeonContTitle.on('click', function () {
-        $(this).next().fadeToggle();
-        $(this).children().toggle();
-    })
+  $accordeonContTitle.on('click', function () {
+    $(this).next().fadeToggle();
+    $(this).children().toggle();
+  })
+
+  $('.s-checkout-form__check-label').on('click', function (el) {
+
+    $('.s-checkout-form__check-label').removeClass('s-checkout-form__check-label--toggleBg')
+
+    if ($(this).closest('input').attr('checked') !== 'checked') {
+      $(this).addClass('s-checkout-form__check-label--toggleBg')
+    }
+
+    if (el.target.getAttribute('value') === 'courier') {
+      $('.show-courier').show(150)
+      $('.show-post').hide(150)
+    } else if (el.target.getAttribute('value') === 'post') {
+      $('.show-courier').hide(150)
+      $('.show-post').show(150)
+    }
+
+  })
 });
